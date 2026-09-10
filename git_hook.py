720b40856c33353dac9d9bf13cd4d97dfe05596b
Keval <keval_devganiya@LAPTOP-3KJFEN5Q.localdomain>
2026-09-10 05:11:55 +0000

    some change in git_hook file

---
+++ git_hook.py
@@ -0,0 +1,13 @@
+"""A module to demonstrate Git pre-commit hooks."""
+
+
+def calculate_sum():
+    """Calculate and print the sum of two numbers."""
+    a = 10
+    b = 30
+    c = a + b
+    print(c)
+
+
+if __name__ == "__main__":
+    calculate_sum()
